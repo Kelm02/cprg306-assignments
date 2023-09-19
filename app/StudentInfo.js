e9f8d4ddae9b347d0155fabbcbeeec2380b9c727
Mitchell Kelm <mitchell.kelm@icloud.com>
2023-09-19 13:45:11 -0600

    finished assignment 2

---
+++ app/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export function StudentInfo() {
+    return (
+        <main>
+            <h3>Mitchell Alexander Kelm</h3>
+            <h3>CPRG 306 F</h3>
+            <Link href="https://github.com/Kelm02/cprg306-assignments.git" >GitHub</Link>
+        </main>
+    );
+}
+
---
+++ app/week2/page.js
@@ -0,0 +1,12 @@
+import { StudentInfo } from "../StudentInfo"
+
+export default function Home() {
+    return (
+      <main>
+  
+        <h1>CPRG 306:My Shopping List</h1>
+        <StudentInfo />
+        
+      </main>
+    )
+  }
