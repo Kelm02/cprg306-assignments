e419767d3531b63689f0906b0cce241b755bc794
Mitchell Kelm <mitchell.kelm@icloud.com>
2023-10-31 13:39:36 -0600

    this is a test

--- app/page.js
+++ app/page.js
@@ -4,15 +4,20 @@ import { StudentInfo } from "./StudentInfo"
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
-
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      
-      <StudentInfo />
-      <Link href="/week2">Click me for Week 2</Link>
-      <Link href="/week3">Click me for Week 3</Link>
-      <Link href="/week4">Click me for Week 4</Link>
-      <Link href="/week5">Click me for Week 5</Link>
+    <main className = "flex min-h-screen flex-col items-center p-24 text-center">
+      <div>
+        <h1 className = "flex flex-col text-xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
+        <StudentInfo /> 
+      </div>
+      <div>
+        <Link href="/week2" className ="bg-sky-700 rounded-md p-2 m-2">Week 2</Link>
+        <Link href="/week3" className ="bg-sky-700 rounded-md p-2 m-2">Week 3</Link>
+        <Link href="/week4" className ="bg-sky-700 rounded-md p-2 m-2">Week 4</Link>
+        <Link href="/week5" className ="bg-sky-700 rounded-md p-2 m-2">Week 5</Link>
+        <Link href="/week6" className ="bg-sky-700 rounded-md p-2 m-2">Week 6</Link>
+        <Link href="/week7" className ="bg-sky-700 rounded-md p-2 m-2">Week 7</Link>
+        <Link href="/week8" className ="bg-sky-700 rounded-md p-2 m-2">Week 8</Link>
+      </div>
 
     </main>
   )
--- app/week5/pages.js
+++ app/week5/pages.js
@@ -3,6 +3,9 @@ import ItemList from "./item-list";
 export default function Page() {
     return (
         <main>
+            <header>
+                <h1>Shopping List</h1>
+            </header>
             <ItemList />
         </main>
     );
